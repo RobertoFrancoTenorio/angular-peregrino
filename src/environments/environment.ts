ce24977793173bdc1019cfc4aafdfee94cfb9c5e
manuel1155 <manuel_1155@hotmail.com>
2021-09-14 23:25:18 -0500

    Commit Inicial DLN-Peregrino

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCaePZTQQZzMscYdDiyP1SB-O-R-BuCT2w",
+    authDomain: "proyecto-peregrino-84c6a.firebaseapp.com",
+    projectId: "proyecto-peregrino-84c6a",
+    storageBucket: "proyecto-peregrino-84c6a.appspot.com",
+    messagingSenderId: "892864448432",
+    appId: "1:892864448432:web:626bdf3a3596a41198b875",
+    measurementId: "G-92RSXG0M6P"
+  }
+};

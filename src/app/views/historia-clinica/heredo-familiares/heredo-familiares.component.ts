9355d406134b92db1c971430f376fad0a527cf06
manuel1155 <manuel_1155@hotmail.com>
2021-10-27 15:00:09 -0500

    ejemplo multiselect checkbox 20211027

--- src/app/views/historia-clinica/heredo-familiares/heredo-familiares.component.ts
+++ src/app/views/historia-clinica/heredo-familiares/heredo-familiares.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
-import { filter, map } from 'rxjs/operators';
 import * as _ from "lodash";
 
 @Component({
--- src/app/views/pacientes/modal-info-pac/modal-info-pac.component.ts
+++ src/app/views/pacientes/modal-info-pac/modal-info-pac.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-info-pac',
@@ -12,14 +13,88 @@ export class ModalInfoPacComponent implements OnInit {
   @Input() detallesBand: boolean = true;
   @Input() antecedentesBand: boolean = false;
   @Input() citasBand: boolean = false;
- 
 
-  constructor(public modalRef: BsModalRef,) { }
+  heredoFamForm: FormGroup;
+  enfermedades = [
+    { id: 'Diabetes', enf: 'Diabetes' },
+    { id: 'Presión', enf: 'Presión' },
+    { id: 'Alergias', enf: 'Alergias' },
+    { id: 'Transfuciones', enf: 'Transfuciones' }
+  ];
+
+
+  constructor(public modalRef: BsModalRef, private fb: FormBuilder) {
+    // Create a FormControl for each available music preference, initialize them as unchecked, and put them in an array
+
+    // Create a FormControl for the select/unselect all checkbox
+   // const selectAllControl = new FormControl(false);
+  
+    // Simply add the list of FormControls to the FormGroup as a FormArray, add the selectAllControl separetely
+    this.heredoFamForm = this.fb.group({
+      padre: new FormArray(this.enfermedades.map(control => new FormControl(false))),
+      madre: new FormArray(this.enfermedades.map(control => new FormControl(false))),
+      hijo: new FormArray(this.enfermedades.map(control => new FormControl(false))),
+      abuelosPaternos: new FormArray(this.enfermedades.map(control => new FormControl(false))),
+      abuelosMaternos: new FormArray(this.enfermedades.map(control => new FormControl(false))),
+      //selectAll: selectAllControl
+    });
+  }
 
   ngOnInit(): void {
     this.currentPaciente['edad'] = this.calcularEdad(this.currentPaciente.pac_f_nacimiento);
+    this.onChanges();
   }
 
+  onChanges(): void {
+    // Subscribe to changes on the selectAll checkbox
+    /*this.heredoFamForm.get('selectAll').valueChanges.subscribe(bool => {
+      console.log(bool);
+
+      this.heredoFamForm
+        .get('enfermedades')
+        .patchValue(Array(this.enfermedades.length).fill(bool), { emitEvent: false });
+    });*/
+
+    // Subscribe to changes on the music preference checkboxes
+
+    this.heredoFamForm.get('padre').valueChanges.subscribe(val => {
+      console.log('abuelosMaternos: '+val);
+    });
+
+    this.heredoFamForm.get('abuelosMaternos').valueChanges.subscribe(val => {
+      console.log('abuelosMaternos: '+val);
+    });
+  }
+
+  submit() {
+    // Filter out the unselected ids
+    const selectedPreferences = this.heredoFamForm.value.padre
+      .map((checked, index) => checked ? this.enfermedades[index].id : null)
+      .filter(value => value !== null);
+    // Do something with the result
+    console.log('Enfermedades padre: ');
+    console.log(selectedPreferences)
+
+    const selectedPreferences2 = this.heredoFamForm.value.madre
+      .map((checked, index) => checked ? this.enfermedades[index].id : null)
+      .filter(value => value !== null);
+    // Do something with the result
+    console.log('Enfermedades madre: ');
+    console.log(selectedPreferences2)
+
+    let model={
+      pac_antecedentes_data : {
+        heredo_familiares: {
+          padre: selectedPreferences,
+          madre: selectedPreferences2
+        }
+      }
+    }
+
+    console.log(model);
+  }
+
+ 
   calcularEdad(fecha: string) {
     var hoy = new Date();
     var cumpleanos = new Date(fecha);
@@ -32,22 +107,22 @@ export class ModalInfoPacComponent implements OnInit {
     return edad;
   }
 
-  activarAntecedentes(){
-    this.citasBand=false;
-    this.detallesBand=false;
-    this.antecedentesBand=true;
+  activarAntecedentes() {
+    this.citasBand = false;
+    this.detallesBand = false;
+    this.antecedentesBand = true;
   }
 
-  activarDetalles(){
-    this.citasBand=false;
-    this.detallesBand=true;
-    this.antecedentesBand=false;
+  activarDetalles() {
+    this.citasBand = false;
+    this.detallesBand = true;
+    this.antecedentesBand = false;
   }
 
-  activarCita(){
-    this.citasBand=true;
-    this.detallesBand=false;
-    this.antecedentesBand=false;
+  activarCita() {
+    this.citasBand = true;
+    this.detallesBand = false;
+    this.antecedentesBand = false;
   }
 
 }
